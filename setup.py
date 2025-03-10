08eed0fd2a61e98386ddd4881e588a28241c0139
rushikeshnayakavadi <rushikeshnayakavadi@gmail.com>
2025-03-10 14:12:52 +0530

    Resolved merge conflict in setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-<<<<<<< HEAD
-    description='This is an end to end mlops proj',
-    author='Rushikesh',
-    license='',
-)
-=======
     description='This is an end to end MLOps Project',
     author='Rushikesh',
     license='',
 )
->>>>>>> 90335a3926aad97e6fbc403bbbdfcb3c4e798cb0
